7f5e245a4d7ed4869035f096655357a69429560e
TEDDY TECH <kibetke4488@gmail.com>
2026-02-15 11:45:04 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -10,7 +10,7 @@ const settings = {
   storeWriteInterval: 10000,
   description: "This is a bot for managing group commands and automating tasks.",
   version: "1.0.8",
-  updateZipUrl: "https://github.com/Teddytech1/botfile/archive/refs/heads/main.zip",
+  updateZipUrl: "https://github.com/Teddytech1/TEDDY-XMD/archive/refs/heads/main.zip",
 };
 
 module.exports = settings;
